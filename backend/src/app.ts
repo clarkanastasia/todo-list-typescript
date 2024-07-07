e9f304b5d88ba36d37ff8ea38760894259ff1104
clarkanastasia <clarkanastasia@outlook.com>
2024-07-07 21:06:41 +0100

    feat: set up express server

---
+++ backend/src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+import routes from "./routes"
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+app.use("/", routes)
+
+app.listen(port, () => {
+    console.log(`Server is live on port ${port}`)
+});
