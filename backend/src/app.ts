be623d549fc8ddf94d7d629968cd374fcd940432
clarkanastasia <clarkanastasia@outlook.com>
2024-07-08 14:00:36 +0100

    fix: update CORS policy

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,22 +2,26 @@ import express from "express";
 import bodyParser from "body-parser";
 import routes from "./routes"
 import cors from "cors";
-
+import router from "./routes";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
+  bodyParser.urlencoded({
+    extended: true,
+  })
 );
 
-app.use(cors({origin: "http://localhost:5173"}));
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+}));
 
-app.use("/", routes)
+app.use("/", routes, router)
 
 app.listen(port, () => {
-    console.log(`Server is live on port ${port}`)
+  console.log(`Server is live on port ${port}`)
 });
